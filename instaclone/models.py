da3276c6fb61597b66d5b879ade54cd793f9a733
philipkariuki <kariukiphil@gmail.com>
2019-07-28 20:05:34 +0300

    creates new mode User'

--- instaclone/models.py
+++ instaclone/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    username = models.CharField(max_length =25)
+    email = models.CharField(max_length =35)
+    password = models.CharField(max_length =35)
