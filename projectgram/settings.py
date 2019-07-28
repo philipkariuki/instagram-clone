9698ac43ee0217b780d8b2cb7a6da28c61ab1139
philipkariuki <kariukiphil@gmail.com>
2019-07-28 19:29:44 +0300

    adds bootstrap4 to settings

--- projectgram/settings.py
+++ projectgram/settings.py
@@ -31,7 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'instaclone'.
+    'instaclone',
+    'bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
