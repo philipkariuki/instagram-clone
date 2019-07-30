2e344398bf30b07cde70a45922b3d1a1aa3903ba
philipkariuki <kariukiphil@gmail.com>
2019-07-30 14:21:56 +0300

    in forms.py creates new form NewPostForm

--- instaclone/forms.py
+++ instaclone/forms.py
@@ -1,13 +1,25 @@
+# from django import forms
+# from django.contrib.auth import authenticate
+# from instaclone.models import Uzer
+# from urllib.request import urlopen
+# from random import randint
 from django import forms
-from django.contrib.auth import authenticate
-from instaclone.models import Uzer
-from urllib.request import urlopen
-from random import randint
+from .models import Image
 
 
 
 
-class UserSignup(forms.Form):
-    username = forms.CharField(label='Username',max_length=30)
-    email = forms.EmailField(label='Email')
-    password = forms.CharField(label='Password',max_length=30)
+# class UserSignup(forms.Form):
+#     username = forms.CharField(label='Username',max_length=30)
+#     email = forms.EmailField(label='Email')
+#     password = forms.CharField(label='Password',max_length=30)
+
+
+
+class NewPostForm(forms.ModelForm):
+    class Meta:
+        model = Image
+        exclude = ['editor', 'pub_date']
+        widgets = {
+            'tags': forms.CheckboxSelectMultiple(),
+        }
--- instaclone/views.py
+++ instaclone/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,redirect
-from .models import Uzer
-from .forms import UserSignup
+from .models import Image
+from .forms import NewImageForm
 from django.http  import HttpResponse, Http404, HttpResponseRedirect
 from registration.backends.simple.views import RegistrationView
 from django.contrib.auth.decorators import login_required
@@ -13,20 +13,20 @@ def welcome(request):
     return render(request, 'index.html')
 
 
-def register(request):
-	if request.method == 'POST':
-		form = UserSignup(request.POST)
-		if form.is_valid():
-			username = form.cleaned_data['username']
-			email = form.cleaned_data['email']
-			password = form.cleaned_data['password']
-			user_signup = Uzer.objects.create_user(username, email, password, username = username,email =email)
-			user_signup.save()
+# def register(request):
+# 	if request.method == 'POST':
+# 		form = UserSignup(request.POST)
+# 		if form.is_valid():
+# 			username = form.cleaned_data['username']
+# 			email = form.cleaned_data['email']
+# 			password = form.cleaned_data['password']
+# 			user_signup = Uzer.objects.create_user(username, email, password, username = username,email =email)
+# 			user_signup.save()
 
-			HttpResponseRedirect('welcome')
-		else:
-			form = UserSignup()
-		return render(request, 'index.html')
+# 			HttpResponseRedirect('welcome')
+# 		else:
+# 			form = UserSignup()
+# 		return render(request, 'index.html')
 
 
 @login_required(login_url='/accounts/login/')
